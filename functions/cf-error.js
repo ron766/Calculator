824e8c2ea79cb348b6a277ad3d4a1383df2bff38
Rohan <37959805+ron766@users.noreply.github.com>
2026-02-02 21:32:31 +0530

    Update cf-error.js [4]

--- functions/cf-error.js
+++ functions/cf-error.js
@@ -1,5 +1,8 @@
-// ❌ Require a module that does not exist
-import nonExistentModule from "./this-file-does-not-exist";
+// ❌ Reference undefined env var during init
+const requiredPort = process.env.REQUIRED_PORT;
+if (!requiredPort) {
+  throw new Error("REQUIRED_PORT is missing");
+}
 
 export default function handler(req, res) {
   res.status(200).send("Never reached");
